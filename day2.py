7afece5781962b25d6d51642f020158d140641aa
movingroot <64212431+movingroot@users.noreply.github.com>
2021-01-21 15:05:53 +0900

    Create day2.py
    tip-calculator

---
+++ day2.py
@@ -0,0 +1,8 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+bill = format(float(input("Please give us receipt.\n")),".2f")
+forEach = format((float(bill) / 5) * 1.12,".2f") 
+
+print(f"Each of you should pay ${forEach}")
