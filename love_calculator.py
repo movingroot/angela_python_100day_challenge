ba1b6fa6fd03d18d8365a259a6a185e286c2ce2c
movingroot <64212431+movingroot@users.noreply.github.com>
2021-01-21 16:10:15 +0900

    Update love_calculator.py

--- love_calculator.py
+++ love_calculator.py
@@ -40,3 +40,24 @@ elif score>=40 and score<=50 :
   print(f"Your score is {score}, you are alright together.")
 else :
   print(f"Your score is {score}.")
+  
+
+# clear approach
+combine = (name1 + name2).lower()
+
+t = combine.count("t")
+r = combine.count("r")
+u = combine.count("u")
+e = combine.count("e")
+l = combine.count("l")
+o = combine.count("o")
+v = combine.count("v")
+
+score = (t + r + u + e)*10 + (l + o + v + e)
+
+if score<10 or score>90 :
+  print(f"Your score is {score}, you go together like coke and mentos.")
+elif score>=40 and score<=50 :
+  print(f"Your score is {score}, you are alright together.")
+else :
+  print(f"Your score is {score}.")
